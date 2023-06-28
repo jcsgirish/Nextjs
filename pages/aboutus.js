861069496dec78f7491a5cc3af46ebe88d9ea9fe
jcsgirish <jcsgirish7@gmail.com>
2023-06-29 00:34:49 +0530

    Next JS

---
+++ pages/aboutus.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+//our-domain.com/aboutus
+
+function page(){
+
+    return <h1>The page</h1>
+}
+
+export default page
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+//our-domain.com/
+
+function Homepage(){
+
+    return <h1>The Homepage</h1>
+}
+
+export default Homepage
